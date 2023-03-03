5a66946bc5d1237372b97a4fedb4c9ef875508c7
farhan0167 <afi225@nyu.edu>
2023-03-03 01:47:36 -0500

    code goals

--- backend/app.py
+++ backend/app.py
@@ -6,5 +6,17 @@ app = Flask(__name__)
 def hello_world():
     return "<p>Hello, World!</p>"
 
+#get cur directory files/folders
+#navigate to a folder
+#go back a folder
+
+#make first commit to remote repo
+#commit changes options-[all, select folders]
+#check git status
+#pull changes
+
+#integrate netlify
+#integrate heroku
+
 if __name__ == "__main__":
     app.run(host='localhost', port=8000)
